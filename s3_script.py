aa042ad0db3be6b40600347053ff40cffb31d512
ShrCode1 <133050819+ShrCode1@users.noreply.github.com>
2024-12-25 07:52:28 +0530

    Update s3_script.py

--- s3_script.py
+++ s3_script.py
@@ -1,5 +1,13 @@
+"""
+A simple script to list S3 buckets using Boto3.
+"""
+
 import boto3
+
 def list_s3_buckets():
+    """
+    List all S3 buckets in the AWS account.
+    """
     s3 = boto3.client('s3')
     response = s3.list_buckets()
     print("S3 Buckets:")
@@ -8,4 +16,3 @@ def list_s3_buckets():
 
 if __name__ == "__main__":
     list_s3_buckets()
-
